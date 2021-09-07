e167267d09a719ee8fa120dd380b28b77649a847
erra-ch <73989943+erra-ch@users.noreply.github.com>
2021-09-07 19:41:34 +0200

    [OOP-12] Added first draft for demo GUI

--- intellij/src/main/java/edu/tda367/SecondaryController.java
+++ intellij/src/main/java/edu/tda367/SecondaryController.java
@@ -1,12 +1,14 @@
 package edu.tda367;
 
 import java.io.IOException;
+
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 
 public class SecondaryController {
 
     @FXML
     private void switchToPrimary() throws IOException {
-        App.setRoot("primary");
+        Platform.exit();
     }
 }

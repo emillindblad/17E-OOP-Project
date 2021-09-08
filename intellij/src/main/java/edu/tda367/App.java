51e9ff6207dea157040d72016f43b82c8633ac59
emillindblad <emil@lindblad.tech>
2021-09-08 20:57:27 +0200

    [OOP-12] Removed hardcoded size

--- intellij/src/main/java/edu/tda367/App.java
+++ intellij/src/main/java/edu/tda367/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 1920, 1080);
+        scene = new Scene(loadFXML("primary"));
         stage.setScene(scene);
         stage.show();
     }

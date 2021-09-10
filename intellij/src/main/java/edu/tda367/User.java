070ae3ea0558dec764c50c363e513ef95ca01b17
Kvalle99 <70636971+Kvalle99@users.noreply.github.com>
2021-09-10 13:49:20 +0200

    Created User with basic functionality

--- intellij/src/main/java/edu/tda367/User.java
+++ intellij/src/main/java/edu/tda367/User.java
@@ -11,15 +11,19 @@ public class User {
     private String userName;
     private String password;
 
+    // proof of concept, needs more information irl
+    private int bankAccount;
+
     //TODO: Owner and customer classes and implementation
 
-    public User(String firstName, String lastName, int phoneNumber, UserAdress userAdress, String userName, String password) {
+    public User(String firstName, String lastName, int phoneNumber, UserAdress userAdress, String userName, String password, int bankAccount) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.phoneNumber = phoneNumber;
         this.userAdress = userAdress;
         this.userName = userName;
         this.password = password;
+        this.bankAccount = bankAccount;
     }
 
     public String getFirstName() {
@@ -44,6 +48,10 @@ public class User {
 
      String getPassword() {
         return password;
+    }
+
+    int getBankAccount() {
+        return bankAccount;
     }
 
 

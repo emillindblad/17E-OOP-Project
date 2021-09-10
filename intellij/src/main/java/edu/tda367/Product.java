4ded95c380fb67b49e5e039efd451dfedb6a2fa7
emillindblad <emil@lindblad.tech>
2021-09-10 14:00:38 +0200

    OOP-19 Added Image attribute to product class

--- intellij/src/main/java/edu/tda367/Product.java
+++ intellij/src/main/java/edu/tda367/Product.java
@@ -1,13 +1,17 @@
 package edu.tda367;
 
+import javafx.scene.image.Image;
+
 public class Product {
     private final String prodName;
     private final Category prodCategory;
     private final String prodDesc;
+    private final Image prodImg;
 
-    public Product(String prodName, Category prodCategory, String prodDesc) {
+    public Product(String prodName, Category prodCategory, String prodDesc, Image prodImg) {
         this.prodName = prodName;
         this.prodCategory = prodCategory;
         this.prodDesc = prodDesc;
+        this.prodImg = prodImg;
     }
 }

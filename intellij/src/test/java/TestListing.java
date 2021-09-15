e4ef1d73ff1cc7d0219cda714131b6f7ba8a5141
emillindblad <emil@lindblad.tech>
2021-09-15 10:57:08 +0200

    Improved test

--- intellij/src/test/java/TestListing.java
+++ intellij/src/test/java/TestListing.java
@@ -16,21 +16,7 @@ public class TestListing {
         LocalDateTime startDate = LocalDateTime.of(2021,9,10,9,0);
         LocalDateTime endDate = LocalDateTime.of(2021,9,11,10,30);
 
-        Duration d  = Duration.between(startDate,endDate);
-        d.getUnits();
-        long d2 = ChronoUnit.HOURS.between(startDate,endDate);
-        System.out.println(d2);
-        assertTrue(1==1);
+        long duration = ChronoUnit.HOURS.between(startDate,endDate);
+        assertTrue(duration==25);
     }
-
-    @Test
-    public void testConstructor() {
-        LocalDateTime startDate = LocalDateTime.of(2021,9,10,9,0);
-        LocalDateTime endDate = LocalDateTime.of(2021,9,11,10,30);
-        Category testCategory = new Category("foo");
-        Listing testListing = new Listing("PRIT Grill",testCategory,"nice",1,1337,startDate,endDate);
-        System.out.println(testListing);
-        assertTrue(1==1);
-    }
-
 }

a1eb1993beb4570600059f0afcac9d86042c7908
florett1 <70637056+florett1@users.noreply.github.com>
2021-09-16 13:30:03 +0200

    Update Booking.java

--- intellij/src/main/java/edu/tda367/Booking.java
+++ intellij/src/main/java/edu/tda367/Booking.java
@@ -2,18 +2,36 @@ package edu.tda367;
 
 public class Booking {
 
-    private final String customerName;
     private BookingState bookingState = BookingState.PENDING;
+    private final User customer;
+    private final Listing listing;
 
-    public Booking(String customerName) {
-        this.customerName = customerName;
-    }
-
-    public String getCustomerName() {
-        return customerName; //Strings are immutable?
+    public Booking(User customer, Listing listing) {
+        this.customer = customer;
+        this.listing = listing;
     }
 
     public BookingState getBookingState() {
         return bookingState;
     }
+
+    private void advanceBookingState() {
+        if (bookingState == BookingState.ACCEPTED) {
+            bookingState = BookingState.PAYED;
+        }
+
+        if (bookingState == BookingState.PENDING) {
+            bookingState = BookingState.ACCEPTED;
+        }
+    }
+
+    private boolean getBookingAccepted() {
+        if (listing.bookingIsAccepted(this)) {
+            advanceBookingState();
+            return true;
+        }
+
+        return false;
+    }
+
 }

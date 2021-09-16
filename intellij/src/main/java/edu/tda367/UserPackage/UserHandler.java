8ad4867320cff0d2a1ba34bbd1b61da352e48c3f
Kvalle99 <70636971+Kvalle99@users.noreply.github.com>
2021-09-16 13:44:17 +0200

    OOP-18, tests for second log-in started

--- intellij/src/main/java/edu/tda367/UserPackage/UserHandler.java
+++ intellij/src/main/java/edu/tda367/UserPackage/UserHandler.java
@@ -1,20 +1,22 @@
 package edu.tda367.UserPackage;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserHandler {
     //TODO implement how/when users-list gets saved to database
-    private List<User> users;
+    private ArrayList<User> users;
     private boolean isAuthenticated;
     private User loggedInUser;
 
     public UserHandler (){
         //TODO implement how list of users gets populated.
         isAuthenticated = false;
+        users = new ArrayList<User>();
     }
 
-    public boolean logIn (String userName, String password) {
-
+    public boolean logIn (String userName, String password) { //can only logg in if no other user is logged in
+        if(!isAuthenticated){
         for (User user : users) {
             String uName = user.getUserName();
             String uPassword = user.getPassword();
@@ -22,9 +24,17 @@ public class UserHandler {
                 isAuthenticated = true;
                 loggedInUser = user;
                 return true; //login successful
+                }
             }
         }
         return false; //login unsuccessful
+    }
+
+    public void logOut () {
+        if(isAuthenticated) {
+            loggedInUser = null;
+            isAuthenticated = false;
+        }
     }
 
     public User getLoggedInUser() {

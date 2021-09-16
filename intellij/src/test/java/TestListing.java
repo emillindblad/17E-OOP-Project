3f2b96ad26d5497ab822165fba7589039779482f
emillindblad <emil@lindblad.tech>
2021-09-16 13:33:01 +0200

    Updated test

--- intellij/src/test/java/TestListing.java
+++ intellij/src/test/java/TestListing.java
@@ -1,21 +1,33 @@
-import edu.tda367.Category;
-import edu.tda367.Listing;
-import edu.tda367.Product;
+import edu.tda367.Listing.*;
+import org.junit.Before;
 import org.junit.Test;
 
-import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
 import static org.junit.Assert.assertTrue;
 
 public class TestListing {
+    private Category testCat;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+
+    @Before
+    public void setup() {
+        this.testCat = new Category("Test category");
+        this.startDate = LocalDateTime.of(2021,9,10,9,0);
+        this.endDate = LocalDateTime.of(2021,9,11,10,30);
+    }
+
+    @Test
+    public void testConstructor() {
+        Listing testListing = new Listing("PRIT Grill",testCat,"Big grill",69,1337,startDate,endDate);
+        assertTrue(testListing.toString().equals(testListing.toString()));
+        assertTrue(testListing.getOrderSate()==ListingState.AVALIBLE);
+    }
 
     @Test
     public void testDuration() {
-        LocalDateTime startDate = LocalDateTime.of(2021,9,10,9,0);
-        LocalDateTime endDate = LocalDateTime.of(2021,9,11,10,30);
-
         long duration = ChronoUnit.HOURS.between(startDate,endDate);
         assertTrue(duration==25);
     }

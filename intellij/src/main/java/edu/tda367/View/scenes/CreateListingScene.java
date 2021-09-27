21e8af13fce1a41c78db6352b3786f466b34359f
emillindblad <emil@lindblad.tech>
2021-09-27 16:01:45 +0200

    [OOP-19] Added dummy categories

--- intellij/src/main/java/edu/tda367/View/scenes/CreateListingScene.java
+++ intellij/src/main/java/edu/tda367/View/scenes/CreateListingScene.java
@@ -1,31 +1,53 @@
 package edu.tda367.View.scenes;
 
 import edu.tda367.Controllers.ListingController;
+import edu.tda367.Listing.ListingHandler;
 import edu.tda367.View.SceneHandler;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class CreateListingScene extends AbstractHyroScene {
     private final ListingController listingController;
+    //private final ListingHandler handler;
 
     @FXML private TextField productName;
     @FXML private TextField productDesc;
     @FXML private TextField prodPrice;
     @FXML private DatePicker prodAvail;
-    @FXML private ComboBox categoriesDropdown;
+    @FXML private ComboBox<String> categoriesDropdown;
 
     public CreateListingScene(SceneHandler handler) throws IOException {
         super("createlisting",handler);
         this.listingController = new ListingController();
+
+        this.categoriesDropdown.getItems().setAll(loadCategories());
         System.out.println("Create Listing now");
+    }
+
+    public ArrayList<String> loadCategories() {
+        ArrayList<String> categories = new ArrayList<>(Arrays.asList("Övrigt","Något annat"));
+        return categories;
+    }
+
+    public void getFormInput() {
+        System.out.println(productName.getText());
+        System.out.println(productDesc.getText());
+        System.out.println(prodPrice.getText());
+        System.out.println(categoriesDropdown.getSelectionModel().getSelectedItem());
     }
 
     @FXML
     public void createListing() {
-
+        getFormInput();
+        System.out.println("Created listing");
+        //handler.createListing();
     }
 }

d81c16ee4836e958ebaec0dfc15864c20b8ecb2b
hanker00 <nilsson.timothy@gmail.com>
2021-10-06 16:06:54 +0200

    Added simple JavaDoc to hyroScene interface

--- intellij/src/main/java/edu/tda367/View/hyroScene.java
+++ intellij/src/main/java/edu/tda367/View/hyroScene.java
@@ -2,7 +2,21 @@ package edu.tda367.View;
 
 import javafx.scene.Scene;
 
+
+/**
+ * Interface that all scenes implemented it includes methods that all scenes should have
+ */
 public interface hyroScene {
+
+    /**
+     * Gets the JavaFX scene from Class
+     * @return
+     */
     Scene getHyroScene();
+
+    /**
+     * Method for updating scene when for example a scene switches.
+     * The application need some way to load new information so that it displays correctly in the GUI these methods are called in the update method.
+     */
     void update();
 }

3edacc70453fbf887b603114559dc705245c6002
emillindblad <emil@lindblad.tech>
2021-10-12 17:58:59 +0200

    Module stuff idk how it works

--- intellij/src/main/java/edu/tda367/Model/Listing/ListingSorter.java
+++ intellij/src/main/java/edu/tda367/Model/Listing/ListingSorter.java
@@ -1,6 +1,8 @@
 package edu.tda367.Model.Listing;
 
 
+import org.apache.commons.text.similarity.CosineDistance;
+
 public class ListingSorter {
-   // CosineDistance cos = new CosineDistance
+   CosineDistance cos = new CosineDistance();
 }
--- intellij/src/main/java/module-info.java
+++ intellij/src/main/java/module-info.java
@@ -4,10 +4,11 @@ module edu.tda367 {
     requires com.google.gson;
     requires java.datatransfer;
     requires java.desktop;
+    requires org.apache.commons.text;
 
     opens edu.tda367 to javafx.fxml;
     opens edu.tda367.Model.UserPackage to com.google.gson;
-    opens edu.tda367.Model.Listing to com.google.gson;
+    opens edu.tda367.Model.Listing to com.google.gson, org.apache.commons.text;
 
     opens edu.tda367.View.scenes to javafx.fxml;
     exports edu.tda367;

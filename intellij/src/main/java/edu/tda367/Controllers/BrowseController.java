89a5b05f615238f0c715a9234db9357b11033bf8
Timothy <nilsson.timothy@gmail.com>
2021-10-12 11:09:25 +0200

    Adding functionality to a singlelisting view

--- intellij/src/main/java/edu/tda367/Controllers/BrowseController.java
+++ intellij/src/main/java/edu/tda367/Controllers/BrowseController.java
@@ -2,15 +2,24 @@ package edu.tda367.Controllers;
 
 import edu.tda367.Listing.Listing;
 import edu.tda367.Listing.ListingHandler;
+import edu.tda367.View.SceneHandler;
 
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 public class BrowseController implements Controller {
-    ListingHandler listingHandler;
+    private ListingHandler listingHandler;
+    private SceneHandler sceneHandler;
 
-    public BrowseController() {
+    public void switchToListing(String productName) throws IOException {
+        Listing listing = listingHandler.getListingByProductName(productName);
+        sceneHandler.switchToListingView(listing);
+    }
+
+
+    public BrowseController(SceneHandler handler) {
         this.listingHandler = new ListingHandler();
+        this.sceneHandler = handler;
     }
 
     public ArrayList<Listing>  getAvailableListings() {

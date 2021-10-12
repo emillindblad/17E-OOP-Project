3b82f721d25b26043c5da7ea2492365446971b1a
erra-ch <73989943+erra-ch@users.noreply.github.com>
2021-10-12 10:05:34 +0200

    Changed BookingHandler to singleton

--- intellij/src/main/java/edu/tda367/Model/Booking/BookingHandler.java
+++ intellij/src/main/java/edu/tda367/Model/Booking/BookingHandler.java
@@ -1,15 +1,26 @@
 package edu.tda367.Model.Booking;
 
+import edu.tda367.Model.JSON.JSONReader;
+import edu.tda367.Model.JSON.JSONWriter;
 import edu.tda367.Model.Listing.Listing;
 import edu.tda367.Model.UserPackage.User;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class BookingHandler {
+    private static BookingHandler instance;
     private final ArrayList<Booking> bookings;
 
-    public BookingHandler() {
-        this.bookings = new ArrayList();
+    private BookingHandler() {
+        bookings = getSavedBookings();
+    }
+
+    public static BookingHandler getInstance() {
+        if (instance == null) {
+            instance = new BookingHandler();
+        }
+        return instance;
     }
 
     public void createBooking(User customer, int userID, Listing listing) {
@@ -28,4 +39,34 @@ public class BookingHandler {
     }
 
     public void advanceBookingState(int bookingIndex) { bookings.get(bookingIndex).advanceBookingState();}
+
+    /**
+     * Gets saved Bookings from database
+     * @author Erik Larsson
+     * @return An ArrayList containing Booking objects
+     *
+     */
+    private ArrayList<Booking> getSavedBookings() {
+        ArrayList<Booking> listingstmp = new ArrayList<>();
+        JSONReader reader = new JSONReader();
+        List<Booking> savedUsers = reader.read(Booking[].class, "bookings");
+        savedUsers.forEach(l ->
+        {
+            if (l == null) {
+                System.out.println("null object in json file");
+            } else {
+                listingstmp.add(l);
+            }
+        });
+        return listingstmp;
+    }
+
+    /**
+     * Writes Bookings to database
+     * @author Erik Larsson
+     */
+    public void writeBookings() {
+        JSONWriter writer = new JSONWriter();
+        writer.write(bookings, "bookings");
+    }
 }

fd6c36dd75a66c5effe7c82dab2ec7058301155b
florett1 <70637056+florett1@users.noreply.github.com>
2021-10-18 10:22:46 +0200

    Adds JavaDocs and removes unused imports

--- intellij/src/main/java/edu/tda367/Controllers/MyListingsController.java
+++ intellij/src/main/java/edu/tda367/Controllers/MyListingsController.java
@@ -1,28 +1,39 @@
 package edu.tda367.Controllers;
 
 //import edu.tda367.Model.Booking.Booking; //can we avoid making Booking a public class?
-import edu.tda367.Model.Booking.BookingHandler;
-import edu.tda367.Model.Listing.Listing;
 import edu.tda367.Model.UserPackage.UserHandler;
 import edu.tda367.View.SceneHandler;
-import edu.tda367.View.scenes.RentingItem;
 import javafx.scene.layout.FlowPane;
-
 import java.util.ArrayList;
 
+/**
+ * MVC Controller for RentingPage
+ */
 public class MyListingsController {
 
     private final SceneHandler sHandler;
     private final UserHandler uHandler = UserHandler.getInstance();
 
+    /**
+     * Constructor
+     * @param handler SceneHandler used to switch scenes
+     */
     public MyListingsController(SceneHandler handler) {
         this.sHandler = handler;
     }
 
+    /**
+     * Switches scene to browse
+     */
     public void goBack() {
         sHandler.switchTo("browse");
     }
 
+    /**
+     * Adds updated RentingItems to FlowPanes
+     * @param listingsPane FlowPane where listings are to be added
+     * @param bookingsPane FlowPane where bookings are to be added
+     */
     public void update(FlowPane listingsPane, FlowPane bookingsPane) {
         updateListings(listingsPane);
         updateBookings(bookingsPane);
--- intellij/src/main/java/edu/tda367/View/scenes/RentingPage.java
+++ intellij/src/main/java/edu/tda367/View/scenes/RentingPage.java
@@ -1,15 +1,15 @@
 package edu.tda367.View.scenes;
 
 import edu.tda367.Controllers.MyListingsController;
-import edu.tda367.Controllers.RentingItemController;
 import edu.tda367.View.SceneHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
 
 import java.io.IOException;
 
+/**
+ * FXML Controller class for RentingPage
+ */
 public class RentingPage extends AbstractHyroScene {
 
     private final MyListingsController controller;
@@ -17,16 +17,30 @@ public class RentingPage extends AbstractHyroScene {
     @FXML private FlowPane listingsPane;
     @FXML private FlowPane bookingsPane;
 
+    /**
+     * Constructor. Sets up JavaFX
+     * @param handler SceneHandler for controller to use for changing scenes
+     * @throws IOException
+     */
     public RentingPage(SceneHandler handler) throws IOException {
         super("rentingpage", handler);
         controller = new MyListingsController(handler);
     }
 
+    /**
+     * Called when scene is switched to
+     * Updates lists of Listings and Bookings
+     * Chains call to controller
+     */
     @Override
     public void update() {
         controller.update(listingsPane, bookingsPane);
     }
 
+    /**
+     * Goes back to browse page
+     * Chains call to controller
+     */
     @FXML
     private void goBack() {
         controller.goBack();

8bff0bb09c3593838ee037109aa7f137c9873b14
emillindblad <emil@lindblad.tech>
2021-10-19 12:23:04 +0200

    Fixed test

--- intellij/src/test/java/edu/tda367/TestListing.java
+++ intellij/src/test/java/edu/tda367/TestListing.java
@@ -109,7 +109,7 @@ public class TestListing {
         String[] data = {"Test Name","Test Desc","69","Övrigt","DummyImgPath"};
         Listing listing = handler.createListing(data,dummyId);
         String[] getData = handler.getListingData(listing.getListingId());
-        assertTrue(Arrays.equals(getData,new String[] {listing.getListingId(),"Test Name","Övrigt","Test Desc","69","AVALIBLE"}));
+        assertTrue(Arrays.equals(getData,new String[] {listing.getListingId(),"Test Name","Övrigt","Test Desc","69","AVALIBLE","DummyImgPath"}));
     }
 
     @Test
